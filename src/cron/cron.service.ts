a305f509c969fda36e8741219d82114a8bfe8e51
Natalia Barros <natalia.vieiramonteiro70@gmail.com>
2023-09-22 19:57:06 -0300

    tentando pegar arquivo na memoria e descompactar

--- src/cron/cron.service.ts
+++ src/cron/cron.service.ts
@@ -2,14 +2,19 @@ import * as cron from 'node-cron';
 import { Injectable } from '@nestjs/common';
 import { createWriteStream, existsSync, mkdirSync } from 'fs';
 import axios from 'axios';
-import fs from 'fs';
+import * as fs from 'fs';
 import { resolve } from 'dns';
 import * as path from 'path';
+import { gunzip } from 'gunzip-file';
+import { log } from 'console';
+import * as zlib from 'zlib';
+import * as pako from 'pako';
+import * as AdmZip from 'adm-zip';
 
 @Injectable()
 export class CronService {
   constructor() {
-    cron.schedule('0 9 * * *', () => {
+    cron.schedule('*/5 * * * * *', () => {
       this.importOpenFoodData();
     });
   }
@@ -28,7 +33,7 @@ export class CronService {
     // for (const fileUrl of fileUrls) {
     // }
     // this.lerUrl('index.txt');
-    this.getFile('https://challenges.coode.sh/food/data/json/index.txt');
+    await this.getFile('https://challenges.coode.sh/food/data/json/index.txt');
   }
 
   //   async lerArquivo(nomeArquivo: string) {
@@ -42,7 +47,8 @@ export class CronService {
     const files = response.data.split('\n').filter((s) => s != '');
 
     for (let i = 0; i < files.length; i++) {
-      await this.downloadFile(files[i]);
+      //await this.downloadFile(files[i]);
+      await this.fetchGzipJSON(files[i]);
     }
 
     // console.log('passei aqui');
@@ -50,31 +56,130 @@ export class CronService {
     // console.log(files[0]);
   }
 
+  fetchGzipJSON = async (file) => {
+    let inflatedJSON = {};
+    const fileUrl = `https://challenges.coode.sh/food/data/json/${file}`;
+    
+    const filePath = './src/teste.zip';
+    try {
+      // const { data } = await axios.get(fileUrl, {
+      //   responseType: 'arraybuffer',
+      //   decompress: true,
+      // });
+
+     // const data = fs.readFileSync(filePath);
+      const zip = new AdmZip(filePath);
+      const zipEntry = zip.getEntry('teste.json');
+      if (!zipEntry) {
+        throw new Error('Arquivo JSON não encontrado no arquivo ZIP.');
+       }
+      const jsonContent = zipEntry.getData().toString('utf8');
+
+      // //inflatedJSON = JSON.parse(pako.inflate(data, { to: 'string' }));
+      inflatedJSON = JSON.parse(pako.inflate(jsonContent, { to: 'string' }));
+    } catch (error) {
+      console.error('could not fetch gzip json', error);
+    }
+    return inflatedJSON;
+  };
+
   async downloadFile(file: string) {
     const fileUrl = `https://challenges.coode.sh/food/data/json/${file}`;
     const downloadFolder = './downloads';
+    let inflatedJSON = {};
 
     if (!existsSync(downloadFolder)) {
       mkdirSync(downloadFolder);
     }
 
     try {
-      const response = await axios.get(fileUrl, { responseType: 'stream' });
-      const fileName = fileUrl.split('/').pop();
-      const filePath = path.resolve(downloadFolder, fileName); // Caminho completo do arquivo
-
-      const fileStream = createWriteStream(filePath);
-      response.data.pipe(fileStream);
-
-      await new Promise((resolve, reject) => {
-        fileStream.on('finish', resolve);
-        fileStream.on('error', reject);
+      const response = await axios.get(fileUrl, {
+        responseType: 'arraybuffer',
+        decompress: true,
+        // headers: {
+        //   'Content-Type': 'application/json',
+        //   'Accept-Enconding': 'gzip, deflate, br',
+        // },
       });
+      console.log(response.data);
+      inflatedJSON = JSON.parse(
+        pako.inflate(response.data, {
+          to: 'string',
+        }),
+      );
+      // .then((resp) => {
+      //   let out;
+      //   console.log(resp.data)
+      //   zlib.gunzip(resp.data, (err, output) => {
+      //     out = output;
+      //   });
+      //   return out;
+      // });
 
-      console.log(`Arquivo ${fileName} baixado com sucesso.`);
+      console.log('entrei aqui', inflatedJSON);
+
+      // zlib.gunzip(response.data, function (error, result) {
+      //   console.log(result.toString());
+      //   return result.toString();
+      // });
+
+      // const fileName = fileUrl.split('/').pop();
+      // const filePath = path.resolve(downloadFolder, fileName); // Caminho completo do arquivo
+
+      //   const fileStream = createWriteStream(filePath);
+      //   response.data.pipe(fileStream);
+
+      //   await new Promise((resolve, reject) => {
+      //     fileStream.on('finish', resolve);
+      //     fileStream.on('error', reject);
+      //   });
+      //   console.log(`Arquivo ${fileName} baixado com sucesso.`);
+      //   //await this.unzipfile(filePath, downloadFolder);
     } catch (error) {
       console.error(`Erro ao baixar o arquivo ${fileUrl}: ${error.message}`);
     }
+    console.log('antes do retorno', inflatedJSON);
+    return inflatedJSON;
+  }
+
+  async unzipfile(zippedfileName: string, unzippedFilePath: string) {
+    console.log(zippedfileName);
+    console.log(unzippedFilePath);
+    const entrada: fs.ReadStream = fs.createReadStream(zippedfileName);
+    const saida = fs.createWriteStream(unzippedFilePath);
+    // const descompactador = zlib.createGunzip();
+    // entrada.pipe(descompactador).pipe(saida);
+
+    // return new Promise((resolve, reject) => {
+    //   saida.on('finish', () => {
+    //     console.log(`Arquivo descompactado para ${unzippedFilePath}`);
+    //     resolve;
+    //   });
+
+    //   descompactador.on('error', (error) => {
+    //     console.error(`Erro ao descompactar o arquivo: ${error.message}`);
+    //     reject(error);
+    //   });
+    // });
+
+    // const downloads = 'downloads';
+    // const partPath = path.split('\\');
+    // const indexDownloads = partPath.indexOf(downloads);
+    // const zippedfileName = partPath.slice(indexDownloads + 1)[0];
+    // const name = zippedfileName.split('.')[0];
+    // const unzippedFilePath = `.\\${downloads}\\${name}.json`; // Use path.join para criar o caminho
+    // const a = `.\\${downloads}\\${zippedfileName}`;
+    // const unzippedFilePath = path.join(
+    //   __dirname,
+    //   'downloads',
+    //   unzippedFileName,
+    // ); // Use path.join para criar o caminho
+    // console.log('caminho completo', path);
+    // console.log('origem ', a);
+    // console.log('destino', unzippedFilePath);
+    //  gunzip(a, unzippedFilePath, () => {
+    //    console.log('gunzip done!');
+    // });
   }
 
   // async lerUrl(file: string) {
